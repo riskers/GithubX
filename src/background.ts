497d5cfb502fbb741ce8751f374c62650a670166
riskers <gaoyibobobo@gmail.com>
2022-03-13 02:51:27 +0800

    popup action

--- src/background.ts
+++ src/background.ts
@@ -5,3 +5,21 @@ chrome.runtime.onMessage.addListener((request) => {
     chrome.runtime.openOptionsPage();
   }
 });
+
+// click pop icon to open option page - main app
+chrome.action.onClicked.addListener(() => {
+  chrome.runtime.openOptionsPage();
+});
+
+chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails) => {
+  // first install
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    // ...
+    chrome.runtime.openOptionsPage();
+  }
+
+  // update version
+  if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    // ....
+  }
+});
