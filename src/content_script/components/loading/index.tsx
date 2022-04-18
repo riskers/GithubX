66b58548ef1567694c6c5a4432209c4f72f28f68
riskers <gaoyibobobo@gmail.com>
2022-04-18 17:17:11 +0800

    clean code

--- src/content_script/components/loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './style.css';
-
-const Loading: React.FC = () => {
-  return <div className="github-plus-loading" />;
-};
-
-export default Loading;
--- src/content_script/pages/stars/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import './style.css';
-import Sidebar from '@/content_script/pages/stars/Sidebar';
-
-const Stars: React.FC = () => {
-  return <Sidebar />;
-};
-
-export default Stars;
